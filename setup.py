178fe28951d7678d5634839d6fb544c02be3accc
Ankush Bhatia <ankush.bhatia@integrand.in>
2018-10-29 15:13:42 +0530

    Added download url and keywords in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version='1.0',
     packages=['docsearch'],
     url='https://github.com/ankushbhatia2/docsearch',
+    download_url="https://github.com/ankushbhatia2/docsearch/archive/1.0.tar.gz",
+    keywords=["information retrieval", "document similarity", "lda", "nlp", "earth movers distance", "jenson shannon"],
     license='Apache License 2.0',
     author='Ankush Bhatia',
     author_email='ankushbhatia02@gmail.com',
