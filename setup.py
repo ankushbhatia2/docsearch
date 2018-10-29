4019740974ee39be0b3282c0fb5b366215f495e5
Ankush Bhatia <ankush.bhatia@integrand.in>
2018-10-29 14:33:51 +0530

    Added a folder for module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='docsearch',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/ankushbhatia2/docsearch',
+    license='Apache License 2.0',
+    author='Ankush Bhatia',
+    author_email='ankushbhatia02@gmail.com',
+    description='Docsearch is a python module for finding similar documents from a large corpus of documents. '
+)
